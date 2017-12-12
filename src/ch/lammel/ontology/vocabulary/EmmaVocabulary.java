5fa54e0c2e05b67de55a113c3cf04a5215e43ec5
Ben <ben>
2017-12-12 06:42:29 +0100

    added Emma voc

---
+++ src/ch/lammel/ontology/vocabulary/EmmaVocabulary.java
@@ -0,0 +1,13 @@
+package ch.lammel.ontology.vocabulary;
+
+public class EmmaVocabulary {
+	private static final String EMMA_BASIC 		= "http://lammel.ch/emma#";
+	public static final String EMMA_DATA_BASE 	= "http://lammel.ch/emma_data#";
+
+	public static final String EMMA_NAMESPACE					= EMMA_BASIC 	+	"Namespace";
+	public static final String NAMESPACE_HAS_URILINK				= EMMA_BASIC 	+	"namespaceHasURILink";
+	public static final String NAMESPACE_HAS_PREFIX				= EMMA_BASIC 	+	"namespaceHasPrefix";
+	public static final String NAMESPACE_IS_ASSIGNED_TO_DATASETID	= EMMA_BASIC 	+	"namespaceIsAssignedToDatasetID";
+	
+}
+
